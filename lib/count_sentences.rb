a62e30fb79bfbe7ca1ac884db9ddec5e623c8e21
dansbands <dansbands@hotmail.com>
2017-11-01 19:40:16 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    string = self.split(/\.|!|\?/)
+    # binding.pry
+    string.select{ |x| x.length > 0 }.count
   end
 end
