9853f7dece25b643b25664aa46242ce49af351ef
George Gore <georgelgore@gmail.com>
2017-11-01 14:13:13 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] == "." ? true : false
   end
 
   def question?
-
+    self[-1] == "?" ? true : false
   end
 
   def exclamation?
-
+    self[-1] == "!" ? true : false
   end
 
   def count_sentences
-
+    exclam = ".!?"
+    chs = self.chars
+    chs.select.with_index{|el, idx| el unless !exclam.include?(el) || chs[idx] == chs[idx + 1]}.count
   end
 end
